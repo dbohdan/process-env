ddb2f6dd03b223e7b1207fd7aad1001bb935732e
dbohdan <dbohdan@dbohdan.com>
2018-02-13 12:48:18 +0200

    refactor: Simplify search in environ
    Get rid of the warning that results from substituting a
    null-terminated string.

--- set-desktop.sh
+++ set-desktop.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
+environ_grep() {
+    awk -v "re=$1" 'BEGIN { RS="\0"; FS="=" } $0 ~ re { print $2 }' "$2"
+}
+
 env_pid="$(pgrep -u "$USER" mate-session)"
-DBUS_SESSION_BUS_ADDRESS="$(grep -z ^DBUS_SESSION_BUS_ADDRESS= "/proc/$env_pid/environ" | sed s/DBUS_SESSION_BUS_ADDRESS=//)"
-DISPLAY="$(grep -z ^DISPLAY= "/proc/$env_pid/environ" | sed s/DISPLAY=//)"
+DBUS_SESSION_BUS_ADDRESS="$(environ_grep DBUS_SESSION_BUS_ADDRESS "/proc/$env_pid/environ")"
+DISPLAY="$(environ_grep DISPLAY "/proc/$env_pid/environ")"
 echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
 echo "DISPLAY=$DISPLAY"
